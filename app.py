dd8fcbc839e7b45e70d66886678f01f3f8043274
Emily202125 <emily@leadfreak.co>
2025-07-19 17:13:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if st.button("Submit"):
             'x-rapidapi-host': "linkedin-scraper-api-real-time-fast-affordable.p.rapidapi.com"
         }
 
-        conn.request("GET", f"/profile/{profile_slug}", headers=headers)
+        conn.request("GET", "/profile/detail?username=neal-mohan", headers=headers)
         res = conn.getresponse()
         data = res.read()
         parsed = json.loads(data.decode("utf-8"))
