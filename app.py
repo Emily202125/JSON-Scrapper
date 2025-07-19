70104f4bc956762149ea2b4a79fc04b8ea1e3063
Emily202125 <emily@leadfreak.co>
2025-07-19 17:10:39 +0530

    new api app.py

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ user_input = st.text_input("Enter LinkedIn Vanity URL (e.g., vidhant-jain)")
 if st.button("Submit"):
     try:
         profile_slug = user_input.strip()
-        conn = http.client.HTTPSConnection("linkedin-profile-and-company-data-api.p.rapidapi.com")
+        conn = http.client.HTTPSConnection("linkedin-scraper-api-real-time-fast-affordable.p.rapidapi.com")
 
         headers = {
             'x-rapidapi-key': "e6274d5593msh7a4e71bb2fe2f44p1552bejsn76fc9770b557",  # Replace with your real key
-            'x-rapidapi-host': "linkedin-profile-and-company-data-api.p.rapidapi.com"
+            'x-rapidapi-host': "linkedin-scraper-api-real-time-fast-affordable.p.rapidapi.com"
         }
 
         conn.request("GET", f"/profile/{profile_slug}", headers=headers)
